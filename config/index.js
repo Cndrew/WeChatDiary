181e0528a308ce19c9fd9ace60c0defaf1e957e0
Cndrew <43087427+Cndrew@users.noreply.github.com>
2022-09-03 12:14:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,13 +6,13 @@ const CONFIG = {
   // ================================高级配置 - 需要修改================================
 
   // 微信公众号的appID
-  APP_ID: "",
+  APP_ID: "wx9fc9fdb36cc3c542",
 
   // 微信公众号的appsecret
-  APP_SECRET: "",
+  APP_SECRET: "f2e9aab9a52d91853abf0d02df020496",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "",
+  TEMPLATE_ID: "swTRJw7_suzFlEuOZD2zDPywRmPLoMPa6HVHGx19GmY",
 
   // 天行API的key
   TXApiKey: "",
@@ -21,23 +21,35 @@ const CONFIG = {
   user: [
     {
       // 男(女)朋友的名字（或昵称或网名）
-      userName: "小沐沐吖",
+      userName: "小红红",
       // 用户列表的`微信号`
-      userId: "",
+      userId: "yuanjinling0601",
       // 星座
-      star: "摩羯座",
+      star: "双子座",
       // 生日 - 格式：08-22
-      birthday: "01-28",
+      birthday: "07-05",
       // 城市 - 格式：支持省市县区
-      city: "夏邑县",
+      city: "黑龙江省哈尔滨市",
     },
+    {
+      // 男(女)朋友的名字（或昵称或网名）
+      userName: "小灰灰",
+      // 用户列表的`微信号`
+      userId: "oWTxF6vITqNTdaTom8o1UkUuMBkg",
+      // 星座
+      star: "射手座",
+      // 生日 - 格式：08-22
+      birthday: "12-013",
+      // 城市 - 格式：支持省市县区
+      city: "黑龙江省哈尔滨市",
+    }
   ],
 
   // 计划旅游日 - 格式：2022-05-20
   tourism: "2022-12-12",
 
   // 恋爱纪念日 - 格式：2022-05-20
-  loveDay: "2022-05-20",
+  loveDay: "2022-08-04",
 
   // ================================功能配置 - 可选修改================================
 
