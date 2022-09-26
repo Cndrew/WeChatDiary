0a3295903d59333c7030f6a98be8f5ce7d414fad
Cndrew <43087427+Cndrew@users.noreply.github.com>
2022-09-26 22:57:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const CONFIG = {
   APP_SECRET: "f2e9aab9a52d91853abf0d02df020496",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "2ZHikM2cPYN4QwfKBEIQr95ZanOEMdXEvKqFHuT3Clc",
+  TEMPLATE_ID: "ehDYTHxbTYOs3F5cexiBH2YAkWaqIcFnCXBfVRf-a2w",
 
   // 天行API的key
   TXApiKey: "028e8c65f21523e1d5825be315e14c17",
